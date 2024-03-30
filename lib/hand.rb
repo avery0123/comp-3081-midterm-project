9ac2ae7e02f8685cfe185520bae7100fd3e1e86b
Avery Oliver <averyaoliver03@gmail.com>
2024-03-30 00:14:36 -0500

    hand and hand_spec finished

--- lib/hand.rb
+++ lib/hand.rb
@@ -8,12 +8,89 @@ class Hand
   end
 
   def evaluate
+    return 'Straight Flush' if straight_flush?
+    return 'Four of a Kind' if four_of_a_kind?
+    return 'Full House' if full_house?
+    return 'Flush' if flush?
+    return 'Straight' if straight?
+    return 'Three of a Kind' if three_of_a_kind?
+    return 'Two Pair' if two_pair?
+    return 'One Pair' if one_pair?
 
-    "High Card"
+    'High Card'
   end
 
   def <=>(other)
+    my_strength = hand_strength(self)
+    other_strength = hand_strength(other)
 
-    0
+    return my_strength <=> other_strength unless my_strength == other_strength
+
+    compare_highest_cards(other)
+  end
+
+  private
+
+  def hand_strength(hand)
+    hand_strengths = {
+      'Straight Flush' => 9,
+      'Four of a Kind' => 8,
+      'Full House' => 7,
+      'Flush' => 6,
+      'Straight' => 5,
+      'Three of a Kind' => 4,
+      'Two Pair' => 3,
+      'One Pair' => 2,
+      'High Card' => 1
+    }
+    evaluate_strength = evaluate
+    hand_strengths[evaluate_strength]
+  end
+
+  def straight_flush?
+    return false unless flush?
+    straight?
+  end
+
+  def four_of_a_kind?
+    card_ranks = @cards.map(&:rank)
+    card_ranks.any? { |rank| card_ranks.count(rank) == 4 }
+  end
+
+  def full_house?
+    card_ranks = @cards.map(&:rank)
+    card_ranks.uniq.size == 2 && (card_ranks.count(card_ranks[0]) == 3 || card_ranks.count(card_ranks[0]) == 2)
+  end
+
+  def flush?
+    suit = @cards.first.suit
+    @cards.all? { |card| card.suit == suit }
+  end
+
+  def straight?
+    card_ranks = @cards.map(&:rank).sort
+    (card_ranks.last - card_ranks.first) == 4 && card_ranks.uniq.size == 5
+  end
+
+  def three_of_a_kind?
+    card_ranks = @cards.map(&:rank)
+    card_ranks.any? { |rank| card_ranks.count(rank) == 3 }
+  end
+
+  def two_pair?
+    card_ranks = @cards.map(&:rank)
+    card_ranks.uniq.size == 3 && card_ranks.count(card_ranks[0]) == 2 && card_ranks.count(card_ranks[4]) == 2
+  end
+
+  def one_pair?
+    card_ranks = @cards.map(&:rank)
+    card_ranks.uniq.size == 4
+  end
+
+  def compare_highest_cards(other)
+    my_highest_card = @cards.max_by { |card| card.rank }
+    other_highest_card = other.cards.max_by { |card| card.rank }
+
+    my_highest_card.rank <=> other_highest_card.rank
   end
 end
--- spec/hand_spec.rb
+++ spec/hand_spec.rb
@@ -2,25 +2,64 @@ require_relative '../lib/hand'
 require_relative '../lib/card'
 
 RSpec.describe Hand do
-  let(:cards) { [Card.new('Hearts', 'Ace'), Card.new('Diamonds', 'King'), Card.new('Clubs', 'Queen'), Card.new('Spades', 'Jack'), Card.new('Hearts', '10')] }
-  let(:hand) { Hand.new(cards) }
+  let(:straight_flush) { Hand.new([Card.new('Hearts', '10'), Card.new('Hearts', 'Jack'), Card.new('Hearts', 'Queen'), Card.new('Hearts', 'King'), Card.new('Hearts', 'Ace')]) }
+  let(:four_of_a_kind) { Hand.new([Card.new('Hearts', 'Ace'), Card.new('Diamonds', 'Ace'), Card.new('Clubs', 'Ace'), Card.new('Spades', 'Ace'), Card.new('Hearts', '10')]) }
+  let(:full_house) { Hand.new([Card.new('Hearts', 'Ace'), Card.new('Diamonds', 'Ace'), Card.new('Clubs', 'Ace'), Card.new('Spades', '10'), Card.new('Hearts', '10')]) }
+  let(:flush) { Hand.new([Card.new('Hearts', '2'), Card.new('Hearts', '4'), Card.new('Hearts', '6'), Card.new('Hearts', '8'), Card.new('Hearts', '10')]) }
+  let(:straight) { Hand.new([Card.new('Hearts', '10'), Card.new('Diamonds', 'Jack'), Card.new('Clubs', 'Queen'), Card.new('Spades', 'King'), Card.new('Hearts', 'Ace')]) }
+  let(:three_of_a_kind) { Hand.new([Card.new('Hearts', 'Ace'), Card.new('Diamonds', 'Ace'), Card.new('Clubs', 'Ace'), Card.new('Spades', '10'), Card.new('Hearts', 'King')]) }
+  let(:two_pair) { Hand.new([Card.new('Hearts', 'Ace'), Card.new('Diamonds', 'Ace'), Card.new('Clubs', '10'), Card.new('Spades', '10'), Card.new('Hearts', 'King')]) }
+  let(:one_pair) { Hand.new([Card.new('Hearts', 'Ace'), Card.new('Diamonds', 'Ace'), Card.new('Clubs', '2'), Card.new('Spades', '3'), Card.new('Hearts', '4')]) }
+  let(:high_card) { Hand.new([Card.new('Hearts', 'Ace'), Card.new('Diamonds', 'King'), Card.new('Clubs', 'Queen'), Card.new('Spades', 'Jack'), Card.new('Hearts', '10')]) }
 
   describe '#initialize' do
     it 'creates a hand with cards' do
-      expect(hand.cards.length).to eq(5)
+      expect(high_card.cards.length).to eq(5)
     end
   end
 
   describe '#evaluate' do
-    it 'evaluates the hand' do
-      expect(hand.evaluate).to eq('High Card')
+    it 'evaluates a straight flush' do
+      expect(straight_flush.evaluate).to eq('Straight Flush')
+    end
+
+    it 'evaluates four of a kind' do
+      expect(four_of_a_kind.evaluate).to eq('Four of a Kind')
+    end
+
+    it 'evaluates a full house' do
+      expect(full_house.evaluate).to eq('Full House')
+    end
+
+    it 'evaluates a flush' do
+      expect(flush.evaluate).to eq('Flush')
+    end
+
+    it 'evaluates a straight' do
+      expect(straight.evaluate).to eq('Straight')
+    end
+
+    it 'evaluates three of a kind' do
+      expect(three_of_a_kind.evaluate).to eq('Three of a Kind')
+    end
+
+    it 'evaluates two pair' do
+      expect(two_pair.evaluate).to eq('Two Pair')
+    end
+
+    it 'evaluates one pair' do
+      expect(one_pair.evaluate).to eq('One Pair')
+    end
+
+    it 'evaluates high card' do
+      expect(high_card.evaluate).to eq('High Card')
     end
   end
 
   describe '#<=>' do
     it 'compares two hands' do
       other_hand = Hand.new([Card.new('Hearts', '2'), Card.new('Diamonds', '3'), Card.new('Clubs', '4'), Card.new('Spades', '5'), Card.new('Hearts', '6')])
-      expect(hand <=> other_hand).to eq(0) # Placeholder - actual comparison logic needs to be implemented
+      expect(high_card <=> other_hand).to eq(0)
     end
   end
 end
