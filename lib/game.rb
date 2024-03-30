1ae5e4424d13a272a41113857b670c5bf4c982b0
Avery Oliver <averyaoliver03@gmail.com>
2024-03-30 00:19:13 -0500

    game and game_spec updated

--- lib/game.rb
+++ lib/game.rb
@@ -1,25 +1,44 @@
 require_relative 'deck'
+require_relative 'hand'
 require_relative 'player'
 
 class Game
-  attr_reader :deck, :players
+  attr_reader :deck, :players, :pot, :current_player
 
-  def initialize(players)
+  def initialize(*names)
     @deck = Deck.new
-    @players = players
+    @players = names.map { |name| Player.new(name, Hand.new(@deck.deal(5))) }
+    @pot = 0
+    @current_player = @players.first
   end
 
-  def deal_initial_hands
-    players.each do |player|
-      player.hand = Hand.new(deck.deal(5))
+  def deal_cards
+    @players.each { |player| player.hand = Hand.new(@deck.deal(5)) }
+  end
+
+  def bet(amount)
+    @pot += amount
+    @current_player.pot -= amount
+  end
+
+  def switch_turn
+    current_index = @players.index(@current_player)
+    next_index = (current_index + 1) % @players.length
+    @current_player = @players[next_index]
+  end
+
+  def discard(cards_to_discard)
+    cards_to_discard.each { |index| @current_player.hand.cards.delete_at(index) }
+    @current_player.hand.cards.concat(@deck.deal(cards_to_discard.length))
+  end
+
+  def winner
+    winning_player = @players.first
+    @players.each do |player|
+      if player.hand > winning_player.hand
+        winning_player = player
+      end
     end
-  end
-
-  def determine_winner
-    # Add logic to determine the winner
-  end
-
-  def play_round
-    # Add logic to play a round of the game
+    winning_player
   end
 end
--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -1,16 +1,54 @@
 require_relative '../lib/game'
-require_relative '../lib/player'
 
 RSpec.describe Game do
-  let(:players) { [Player.new(Hand.new([])), Player.new(Hand.new([]))] }
-  let(:game) { Game.new(players) }
+  let(:game) { Game.new("Player 1", "Player 2") }
 
   describe '#initialize' do
-    it 'creates a game with players and a deck' do
+    it 'creates a game with a deck and players' do
+      expect(game.deck).to be_instance_of(Deck)
       expect(game.players.length).to eq(2)
-      expect(game.deck).to be_an_instance_of(Deck)
     end
   end
 
-  # Add tests for deal_initial_hands, determine_winner, and play_round methods
+  describe '#deal_cards' do
+    it 'deals cards to each player' do
+      game.deal_cards
+      expect(game.players[0].hand.cards.length).to eq(5)
+      expect(game.players[1].hand.cards.length).to eq(5)
+    end
+  end
+
+  describe '#bet' do
+    it 'adds the bet amount to the pot and deducts from the current player' do
+      game.bet(100)
+      expect(game.pot).to eq(100)
+      expect(game.players[0].pot).to eq(900)
+    end
+  end
+
+  describe '#switch_turn' do
+    it 'switches the current player' do
+      game.switch_turn
+      expect(game.current_player).to eq(game.players[1])
+    end
+  end
+
+  describe '#discard' do
+    it 'discards cards from the current player\'s hand and deals new cards' do
+      game.discard([0, 1])
+      expect(game.current_player.hand.cards.length).to eq(5)
+    end
+  end
+
+  describe '#winner' do
+    it 'determines the winner based on hand strength' do
+      game.players[0].hand = Hand.new([
+        Card.new('Hearts', '2'), Card.new('Hearts', '3'), Card.new('Hearts', '4'), Card.new('Hearts', '5'), Card.new('Hearts', '6')
+      ])
+      game.players[1].hand = Hand.new([
+        Card.new('Hearts', '10'), Card.new('Diamonds', 'Jack'), Card.new('Clubs', 'Queen'), Card.new('Spades', 'King'), Card.new('Hearts', 'Ace')
+      ])
+      expect(game.winner).to eq(game.players[1])
+    end
+  end
 end
